b03da980c760f334df1672d3ea2733bf572ab724
snowmerak <snowmerak@outlook.com>
2022-12-13 21:41:16 +0900

    make router

---
+++ router.go
@@ -0,0 +1,55 @@
+package lates
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/a-h/templ"
+	"github.com/julienschmidt/httprouter"
+)
+
+func (l *Lates) Get(path string, handler func(c *Context) templ.Component) {
+	l.AddRouter("GET", path, handler)
+}
+
+func (l *Lates) Post(path string, handler func(c *Context) templ.Component) {
+	l.AddRouter("POST", path, handler)
+}
+
+func (l *Lates) Put(path string, handler func(c *Context) templ.Component) {
+	l.AddRouter("PUT", path, handler)
+}
+
+func (l *Lates) Delete(path string, handler func(c *Context) templ.Component) {
+	l.AddRouter("DELETE", path, handler)
+}
+
+func (l *Lates) Patch(path string, handler func(c *Context) templ.Component) {
+	l.AddRouter("PATCH", path, handler)
+}
+
+func (l *Lates) AddRouter(method string, path string, handler func(c *Context) templ.Component) error {
+	method = strings.ToUpper(method)
+	switch method {
+	case "GET":
+	case "POST":
+	case "PUT":
+	case "DELETE":
+	case "PATCH":
+	default:
+		return errors.New("invalid method")
+	}
+
+	l.router.Handle(method, path, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		c := &Context{
+			r: r,
+			p: p,
+			w: w,
+		}
+		comp := handler(c)
+		templ.Handler(comp).ServeHTTP(w, r)
+	})
+
+	return nil
+}
